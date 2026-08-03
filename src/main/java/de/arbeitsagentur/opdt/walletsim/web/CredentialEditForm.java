3e330bdff06ab48b3575660eda2b0d243d79d620
Dominik Schlosser <dominik.schlosser@gmail.com>
2026-08-03 14:12:06 +0200

    feat: add wallet overview with ad-hoc credential creation and revocation ui
    Signed-off-by: Dominik Schlosser <dominik.schlosser@gmail.com>

---
+++ src/main/java/de/arbeitsagentur/opdt/walletsim/web/CredentialEditForm.java
@@ -0,0 +1,51 @@
+package de.arbeitsagentur.opdt.walletsim.web;
+
+/** Form backing bean for creating or cloning a credential in the UI. */
+public class CredentialEditForm {
+
+    private String id;
+    private String name;
+    private String vct;
+    private int validityDays = 365;
+    private String claimsJson;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVct() {
+        return vct;
+    }
+
+    public void setVct(String vct) {
+        this.vct = vct;
+    }
+
+    public int getValidityDays() {
+        return validityDays;
+    }
+
+    public void setValidityDays(int validityDays) {
+        this.validityDays = validityDays;
+    }
+
+    public String getClaimsJson() {
+        return claimsJson;
+    }
+
+    public void setClaimsJson(String claimsJson) {
+        this.claimsJson = claimsJson;
+    }
+}
--- src/main/java/de/arbeitsagentur/opdt/walletsim/web/HomeController.java
+++ src/main/java/de/arbeitsagentur/opdt/walletsim/web/HomeController.java
@@ -1,13 +1,30 @@
 package de.arbeitsagentur.opdt.walletsim.web;
 
+import de.arbeitsagentur.opdt.walletsim.credentials.CredentialStore;
+import de.arbeitsagentur.opdt.walletsim.credentials.StoredCredential;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    public record CredentialCard(StoredCredential credential, int status) {}
+
+    private final CredentialStore store;
+
+    public HomeController(CredentialStore store) {
+        this.store = store;
+    }
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        List<CredentialCard> cards = store.findAll().stream()
+                .map(credential -> new CredentialCard(
+                        credential, store.statusOf(credential.id()).orElse(0)))
+                .toList();
+        model.addAttribute("cards", cards);
         return "index";
     }
 }
